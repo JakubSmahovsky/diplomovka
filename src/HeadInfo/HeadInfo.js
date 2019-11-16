43ecf74f7d4a7f471ada697d09ee903c95b82d8e
JakubSmahovsky <jakubsmahovsky@gmail.com>
2019-11-16 14:34:36 +0100

    style head info

--- src/HeadInfo/HeadInfo.js
+++ src/HeadInfo/HeadInfo.js
@@ -8,25 +8,26 @@ export default class HeadInfo extends Component {
   render () {
     return (
       <div className="HeadInfo">
+        <h2 className="Title">Základné informácie</h2>
         <table className="Temp_info">
           <tr>
-            <th> Meno: </th>
+            <th> Meno </th>
             <th> Jakub Šmahovský </th>
           </tr>
           <tr>
-            <td> Mail: </td>
+            <td> Mail </td>
             <td> jakubsmahovsky@gmail.com </td>
           </tr>
           <tr>
-            <td> Školiteľ: </td> 
+            <td> Školiteľ </td> 
             <td> doc. RNDr. Martin Stanek PhD. (môže sa zmeniť) </td>
           </tr>
           <tr>
-            <td> Téma: </td>
+            <td> Téma </td>
             <td> Znenie zatiaľ nemám ale cieľ je prispieť do aplikácie Verifpal</td>
           </tr>
           <tr>
-            <td> Stav: </td>
+            <td> Stav </td>
             <td> Komunikácia s tvorcom aplikácie Verifpal</td>
           </tr>
 
